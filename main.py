a34572d06781802b7aa418f2a2eed470ca9962d4
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-08-28 16:56:19 +0400

    Minor code refactoring

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 print("Please, input a few numbers for creating a list: ")
-l0 = list(map(int, input().split()))
-print(l0)
 
-def ListReverse(List = l0):
-    l1 = l0[::-1]
-    return l1
+source_list = list(map(int, input().split()))  # Enter a source list
+print(source_list)
+
+
+def ListReverse(List=source_list):
+    """Function that reverse a list"""
+    destination_list = source_list[::-1]
+
+    return destination_list
+
 
 if __name__ == "__main__":
     print(ListReverse())
