fd165a50b2036a7e7c43ef08d144b2c0be44969d
Rensys <39058702+NikitaKolotushkin@users.noreply.github.com>
2020-10-11 18:39:20 +0400

    Update README.md

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ print("Please, input a few numbers for creating a list: ")
 source_list = list(map(int, input().split()))  # Enter a source list
 print(source_list)
 
-
 def ListReverse(List=source_list) -> list:
     """Function that reverse a list"""
     destination_list = List[::-1]
